d0cbf2f17da940745efde9b9d004c10ba34fd8e2
Singh-Gagan-Deep <gagandeep.zversal@gmail.com>
2022-05-07 12:04:38 +0530

    display data paggination and search by rocket name

---
+++ src/views/Home/Result.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function BasicCard({ launch }) {
+  return (
+    <Card sx={{}}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Flight No.: {launch.flight_number}
+        </Typography>
+        <Typography variant="h5" component="div">
+          {launch.mission_name}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Rocket: {launch.rocket.rocket_name}
+        </Typography>
+        <Typography variant="body2">
+          Launch Year: {launch.launch_year}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,8 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchLaunches, launchesSelector } from "../../slices/launches";
+import { Container, Grid, TablePagination, TextField } from "@mui/material";
+import Result from "./Result";
+
+const applyPagination = (launches, page, limit) => {
+  return launches.slice(page * limit, page * limit + limit);
+};
+
+const applyFilters = (launches, query) => {
+  return launches.filter((launch) => {
+    let matches = true;
+
+    if (query) {
+      let containsQuery = false;
+
+      if (
+        launch.rocket.rocket_name.toLowerCase().includes(query.toLowerCase())
+      ) {
+        containsQuery = true;
+      }
+
+      if (!containsQuery) {
+        matches = false;
+      }
+    }
+
+    return matches;
+  });
+};
 
 const Home = () => {
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(9);
+  const [query, setQuery] = useState("");
+
   const dispatch = useDispatch();
 
   const { launches, loading, hasErrors } = useSelector(launchesSelector);
@@ -13,7 +45,72 @@ const Home = () => {
 
   console.log("launches: ", launches);
 
-  return <div>index</div>;
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleLimitChange = (event) => {
+    setLimit(parseInt(event.target.value));
+  };
+
+  const handleQueryChange = (event) => {
+    event.persist();
+    setQuery(event.target.value);
+  };
+
+  const filteredLaunches = applyFilters(launches, query);
+
+  const paginatedLaunches = applyPagination(filteredLaunches, page, limit);
+
+  if (loading) {
+    return (
+      <Container>
+        <h2>Loading...</h2>
+      </Container>
+    );
+  }
+
+  if (hasErrors) {
+    return (
+      <Container>
+        <h2>Somethig went wrong</h2>
+      </Container>
+    );
+  }
+
+  return (
+    <Container maxWidth="md">
+      <Grid container>
+        <Grid item>
+          <TextField
+            id="standard-basic"
+            label="Search by rocket name"
+            variant="standard"
+            onChange={handleQueryChange}
+            value={query}
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        {paginatedLaunches.map((launch, index) => {
+          return (
+            <Grid key={index} mt={5} item xs={4} sm={4} md={4}>
+              <Result launch={launch} />
+            </Grid>
+          );
+        })}
+      </Grid>
+      <TablePagination
+        component="div"
+        count={launches.length}
+        onPageChange={handlePageChange}
+        onChangeRowsPerPage={handleLimitChange}
+        page={page}
+        rowsPerPage={limit}
+        rowsPerPageOptions={[5, 10, 25]}
+      />
+    </Container>
+  );
 };
 
 export default Home;
