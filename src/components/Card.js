e80de6ceeb3119680a83256dc23790f2a512c249
Singh-Gagan-Deep <gagandeep.zversal@gmail.com>
2022-05-08 12:49:19 +0530

    mission name length fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function BasicCard({ launch }) {
   return (
@@ -12,9 +13,13 @@ export default function BasicCard({ launch }) {
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Flight No.: {launch.flight_number}
         </Typography>
-        <Typography variant="h5" component="div">
-          {launch.mission_name}
-        </Typography>
+        <Tooltip title={launch.mission_name} arrow>
+          <Typography variant="h5" component="div">
+            {launch.mission_name.length < 14
+              ? launch.mission_name
+              : `${launch.mission_name.substring(0, 14)}...`}
+          </Typography>
+        </Tooltip>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
           Rocket: {launch.rocket.rocket_name}
         </Typography>
